0e479c695776f2480bb77a1ee3cba913fc9fdc87
sahrul6121 <sahrulramadan6121@gmail.com>
2022-07-31 15:46:59 +0700

    feat(response-class): add setter and getter on success response class

--- src/main/java/base/project/restapi/classes/SuccessPaginateResponse.java
+++ src/main/java/base/project/restapi/classes/SuccessPaginateResponse.java
@@ -1,11 +1,15 @@
 package base.project.restapi.classes;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class SuccessPaginateResponse {
     private String message;
--- src/main/java/base/project/restapi/classes/SuccessSingleResponse.java
+++ src/main/java/base/project/restapi/classes/SuccessSingleResponse.java
@@ -1,9 +1,13 @@
 package base.project.restapi.classes;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class SuccessSingleResponse {
     private String message;
